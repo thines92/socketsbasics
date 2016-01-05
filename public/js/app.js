3b34a9a6f05a5b4a76ed5a9162f600d7be8af2ef
Tyler Hines <tylerhines1992@gmail.com>
2016-01-05 16:03:20 -0500

    Add emit functionality

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Conneted to socket.io server!');
+});
+
+socket.on('message', function (message) {
+	console.log('New Message: ');
+	console.log(message.text);
 })
